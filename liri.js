da8c560f98a7c5df8f73b880b0bd3682a25bdfd8
gabriellebyers <gabriellembyers@gmail.com>
2020-07-08 20:15:56 -0500

    finished

--- liri.js
+++ liri.js
@@ -12,81 +12,144 @@ var fs = require("fs");
 
 
 
+// Make it so liri.js can take in one of the following commands:
 
-var getArtistNames = function(artist) {
+// concert-this
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
+
+
+
+
+
+var ArtistNames = function(artist) {
     return artist.name;
   };
+  
 
+  var spotifythissong = function(songName) {
+    if (songName === undefined) {
+      songName = "Take Care";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+  
+        var songs = data.tracks.items;
+  
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(ArtistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+        }
+      }
+    );
+  };
+  
+  var concerthis = function(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryURL).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        if (!jsonData.length) {
+          console.log("No results found.");
+          return;
+        }
+  
+        console.log("Upcoming concerts:");
+  
+        for (var i = 0; i < jsonData.length; i++) {
+          var show = jsonData[i];
+  
+     
+          console.log(
+            show.venue.city + "," + show.venue.region +  " at " +  show.venue.name + " " + moment(show.datetime).format("MM/DD/YYYY")
+          );
+        }
+      }
+    );
+  };
+  
+  // Function for running a Movie Search
+  var moviethis = function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Titanic";
+    }
+  
+    var urlHit =
+      "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  
+    axios.get(urlHit).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      }
+    );
+  };
+  
+ 
+  var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+  
+      var dataArr = data.split(",");
+  
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };
+  
 
+  var pick = function(caseData, functionData) {
+    switch (caseData) {
+    case "concert-this":
+      concerthis(functionData);
+      break;
+    case "spotify-this-song":
+      spotifythissong(functionData);
+      break;
+    case "movie-this":
+      moviethis(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+    }
+  };
+  
 
-
-Make it so liri.js can take in one of the following commands:
-
-concert-this
-
-spotify-this-song
-
-movie-this
-
-do-what-it-says
-
-
-
-
-node liri.js concert-this <artist/band name here>
-
-This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-
-Name of the venue
-
-Venue location
-
-Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-Important: There is no need to sign up for a Bands in Town api_id key. Use the codingbootcamp as your app_id. For example, the URL used to search for "Celine Dion" would look like the following:
-
-https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp
-node liri.js spotify-this-song '<song name here>'
-
-This will show the following information about the song in your terminal/bash window
-
-Artist(s)
-
-The song's name
-
-A preview link of the song from Spotify
-
-The album that the song is from
-
-If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-
-
-node liri.js movie-this '<movie name here>'
-
-This will output the following information to your terminal/bash window:
-
-  * Title of the movie.
-  * Year the movie came out.
-  * IMDB Rating of the movie.
-  * Rotten Tomatoes Rating of the movie.
-  * Country where the movie was produced.
-  * Language of the movie.
-  * Plot of the movie.
-  * Actors in the movie.
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
-It's on Netflix!
-
-You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
-
-node liri.js do-what-it-says
-
-Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-
-Edit the text in random.txt to test out the feature for movie-this and concert-this.
+  var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+  };
+  
+  runThis(process.argv[2], process.argv.slice(3).join(" "));
